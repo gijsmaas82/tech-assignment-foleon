d072cc6e6a8b6b985930d1cf006769089479a422
gijsmaas82 <gijsmaas82@outlook.com>
2019-11-10 22:40:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import PhotoGallery from './Components/PhotoGallery';
 import SearchBar from './Components/SearchBar';
-import PaginationButtons from './Components/PaginationButtons';
+
 
 function App() {
   return (
     <div className="App">
       <SearchBar />
-      <PhotoGallery />
-      <PaginationButtons />
+      <PhotoGallery />     
     </div>
   );
 }
