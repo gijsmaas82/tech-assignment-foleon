eb468afa729332937ee7e2e00112c5d8b7aecc48
gijsmaas82 <gijsmaas82@outlook.com>
2019-11-10 22:11:01 +0100

    Make photogallery

--- src/Components/PhotoGallery/view.js
+++ src/Components/PhotoGallery/view.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react'
+import { Figure, Spinner, Jumbotron } from 'react-bootstrap'
 
 export default class PhotoGalleryView extends Component {
   render() {
     return (
-      <div>
+      <div >
+        {!this.props.photos && !this.props.searchTag && 
+          <Jumbotron fluid style={{ display:"flex", flexDirection:"column", alignItems:"center"}}>
+            <h3>Welcome to this Photo Search App</h3>
+            <p>Please type something that you are interested in and start searching for cool images</p>
+
+          </Jumbotron>
+        }
+        {!this.props.photos && this.props.searchTag &&
+          <Spinner animation="border" role="status">
+            <span className="sr-only">Loading...</span>
+          </Spinner>
+        }
+        {this.props.photos && 
+            <div>
+              <Jumbotron fluid style={{ display:"flex", justifyContent:"center"}}>
+                <h3>Results for {this.props.searchTag}</h3>
+
+              </Jumbotron>
+            <div style={{ display:"flex", flexDirection:"column", alignItems:"center"}}>{this.props.photos.map(photo => {
+            return <Figure>
+              <Figure.Image rounded width={500}
+              src={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`} alt="pic" />
+            </Figure>
+          })}</div></div>
+          
+        }
         
       </div>
     )
--- src/actions.js
+++ src/actions.js
@@ -12,7 +12,7 @@ function setPhotos(payload) {
 export const getPhotos = (tag) => dispatch => {  
 
   request('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=6c116b8d53864e12de7b50956b88932e&format=json&nojsoncallback=1')
-      .query(`per_page=10&tags=${tag}`)
+      .query(`per_page=5&tags=${tag}`)
       .then(response => {
         const action = setPhotos(response.body.photos.photo )
         dispatch(action)
